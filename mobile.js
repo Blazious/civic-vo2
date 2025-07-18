6c1833f387d9bebd067b1cb579bc8675289ee285
Blazious <blaziousmwambuwa@gmail.com>
2025-07-18 09:19:36 +0300

    3 commit

--- mobile.js
+++ mobile.js
@@ -1,5 +1,5 @@
 // === CONFIGURATION ===
-const BASE_URL = 'https://3f2d297d5513.ngrok-free.app';
+const BASE_URL = 'https://361e22cb78db.ngrok-free.app';
 const DETECTION_API_URL = 'https://detect.roboflow.com/pothole-detection-lwf9u/3';
 const API_KEY = 'zwRLkWFX34uJ2UPRjUYC';
 const MAX_SESSION_MINUTES = 20;
