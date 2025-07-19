5e23e9569ff32bdace44ecb2c74f94edc7566b5d
Blazious <blaziousmwambuwa@gmail.com>
2025-07-19 18:25:15 +0300

    changed

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // === CONFIGURATION ===
-const BASE_URL = 'https://3f2d297d5513.ngrok-free.app';
+const BASE_URL = 'https://c1d1776d61be.ngrok-free.app';
 const DETECTION_API_URL = 'https://detect.roboflow.com/pothole-detection-lwf9u/3';
 const API_KEY = 'zwRLkWFX34uJ2UPRjUYC';
 const MAX_SESSION_MINUTES = 20;
